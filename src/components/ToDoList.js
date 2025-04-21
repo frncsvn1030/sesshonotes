0918194f637b0c56a4256219f131c5278811b3ae
Francis_DEV <f.vino@qstrike.com>
2025-04-21 18:30:08 +0800

    Added: Function to 'Add' & 'Delete' items from the list

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const ToDoList = () => {
+    const[items, setItems] = useState({
+        todo: '',
+        todolist: []
+    })
+    const { todo, todolist } = items
+
+    const handleChange = (e) => {
+        setItems({...items, todo: e.target.value
+        });
+    }
+
+    const addToList = () => {
+        if (todo.trim() !== '') {                   // makes sure empty value won't be added
+            const list = [todo, ...todolist];       // add new list + copy old list
+            setItems({
+                ...items,
+                todo: '',                           // clear input after adding
+                todolist: list                      // replace todolist with the new list (pati ung bago naadd)
+            });
+        }
+    }
+
+    const handleDelete = (index) => { 
+        const list = [...todolist]
+        list.splice(index, 1)
+
+        setItems({
+            ...items,
+            todolist: list
+        })
+    }
+
+      
     return (
         <div className="todolist-main">
             <div className="content uk-card uk-card-default uk-card-body">
@@ -13,32 +47,43 @@ const ToDoList = () => {
                                 className="uk-input" 
                                 type="text" 
                                 placeholder="What do you need to do today?" 
-                                aria-label="Input"
+                                value={todo}
+                                onChange={handleChange}
                             />
                         </div>
-                        <button className="uk-button uk-button-primary uk-margin-small uk-margin-remove@m uk-width-1-1 uk-width-auto@m">ADD</button>
+                        <button 
+                            className="uk-button uk-button-primary uk-margin-small uk-margin-remove@m uk-width-1-1 uk-width-auto@m" onClick={addToList}>ADD</button>
                     </div>
                 </div>
 
-                {/* Filter options */}
+                {/* filter options */}
                 <div className="uk-margin">
                     <ul className="uk-subnav uk-subnav-pill">
-                        <li className="uk-active"><a href="#">All</a></li>
-                        <li><a href="#">Pending</a></li>
-                        <li><a href="#">Completed</a></li>
+                        <li className="uk-active"><a href="/">All</a></li>
+                        <li><a href="/">Pending</a></li>
+                        <li><a href="/">Completed</a></li>
                     </ul>
                 </div>
-
-                {/* Todo list items */}
-                <div className="uk-margin">
-                    <div className="uk-flex uk-flex-middle uk-flex-between uk-margin-small">
-                        <div className="uk-flex uk-flex-middle">
-                            <input className="uk-checkbox" type="checkbox" />
-                            <span className="uk-margin-small-left">Do my project</span>     
+                
+                {/* to do list */}
+                {
+                    todolist.length > 0 ? (
+                        todolist.map((value, index) => (
+                        <div className="uk-margin" key={index}>
+                            <div className="uk-flex uk-flex-middle uk-flex-between uk-margin-small">
+                                <div className="uk-flex uk-flex-middle">
+                                    <input className="uk-checkbox" type="checkbox" />
+                                    <span className="uk-margin-small-left">{value}</span>     
+                                </div>
+                                <a href="#" className="uk-text-danger" uk-icon="icon: close" onClick={() => handleDelete(index)}>X</a>
+                            </div>
                         </div>
-                        <a href="#" className="uk-text-danger" uk-icon="icon: close">X</a>
-                    </div>
-                </div>
+                        ))
+                    ) : (
+                        <span>No records found!</span>
+                    )
+                }
+
             </div>
         </div>
     )
